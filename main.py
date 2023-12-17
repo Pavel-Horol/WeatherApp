f3ac4aca1069b6541281ba632af81485b7f7a45b
Khorolskij Pavlo <pashahorolskij4@gamil.com>
2023-12-17 14:32:17 +0200

    init proj

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-import requests
 
+
+
+
+
+
+
+
+
+
+
+import requests
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+import sys
 API_KEY = "bb0033061aaab3054af8bbbd53335300"
 city = "Dnipro,ua"
+
+lat, lon = 46.482952, 30.712481
 def kelvin_to_celsius(kelvin_temperature):
     return kelvin_temperature - 273.15
 def get_weather_data(url):
@@ -18,6 +32,30 @@ def get_weather_data(url):
         }
     except Exception as e:
         print("Error", e)
-url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
 weather_data = get_weather_data(url)
-print(f"1. Місто: {weather_data['city']}\n2. Температура: {weather_data['temp_celsius']} \n3. Опис погоди: {weather_data['weather_desc']}")
+# print(f"1. Місто: {weather_data['city']}\n2. Температура: {weather_data['temp_celsius']} \n3. Опис погоди: {weather_data['weather_desc']}")
+
+app = QApplication([])
+window = QWidget()
+window.resize(400, 300)
+
+try:
+    with open("./style.css", 'r') as styles:
+        style_sheet = styles.read()
+except:
+    print("Error file was not found")
+
+app.setStyleSheet(style_sheet)
+city_label = QLabel(f"City: {weather_data['city']}")
+temp_label = QLabel(f"Temperature: {weather_data['temp_celsius']}")
+weather_label = QLabel(f"Weather: {weather_data['weather_desc']}")
+layout = QVBoxLayout()
+
+layout.addWidget(city_label)
+layout.addWidget(temp_label)
+layout.addWidget(weather_label)
+
+window.setLayout(layout)
+window.show()
+sys.exit(app.exec_())
